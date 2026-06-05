92ac6311b0bc3630bc1e5cb891e44ce195b9fd4c
e2506243 <sichardlomme@gmail.com>
2026-06-05 11:21:08 +0200

    mise à jour

--- appsabs/groupes/views.py
+++ appsabs/groupes/views.py
@@ -1,7 +1,22 @@
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect, HttpResponse
 from .models import Groupe
 from .forms import GroupeForm
+from . import models
+
+
+def ajout(request):
+    if request.method == "POST": # arrive en cas de retour sur cette page après unesaisie invalide on récupère donc les données. Normalement nous ne devrions pas passer par ce chemin la pour le traitement des données
+        form = GroupeForm(request)
+        if form.is_valid(): # validation du formulaire.
+            groupes = form.save() # sauvegarde dans la base
+            return render(request,"appsabs/affiche.html",{"appsabs" : Livre}) #envoie vers une page d'affichage du bibliothequeapp créé
+        else:
+            return render(request,"appsabs/ajout_categorie.html",{"form": form})
+    else :
+        form = GroupeForm() # création d'un formulaire vide
+        return render(request,"appsabs/ajout_categorie.html",{"form" : form})
 
 class GroupeListView(ListView):
     model = Groupe
@@ -15,12 +30,21 @@ class GroupeCreateView(CreateView):
     success_url = reverse_lazy('groupe-liste')
 
 class GroupeUpdateView(UpdateView):
-    model = Groupe
+    model = models.Groupe.objects.get(pk=id)
     form_class = GroupeForm
-    template_name = 'groupes/form.html'
-    success_url = reverse_lazy('groupe-liste')
+    if lform.is_valid():
+        Groupe = form_class.save(commit=False)
+        Groupe.id = id
+        Groupe.save()
+        template_name = 'groupes/form.html'
+        success_url = reverse_lazy('groupe-liste')
+        return HttpResponseRedirect("/bibliothequeapp/")  # plutot que d'avoir un gabarit
 
 class GroupeDeleteView(DeleteView):
-    model = Groupe
+
     template_name = 'groupes/confirm_delete.html'
+    groupe = models.Groupe.objects.get(pk=id)
+    livre.delete()
     success_url = reverse_lazy('groupe-liste')
+    return HttpResponseRedirect('/bibliothequeapp/liste/')
+
