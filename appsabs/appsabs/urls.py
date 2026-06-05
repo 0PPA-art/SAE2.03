edb30052127b04bad552cced451d04961e63d605
e2506243 <sichardlomme@gmail.com>
2026-06-05 09:33:21 +0200

    mise à jour

--- appsabs/appsabs/urls.py
+++ appsabs/appsabs/urls.py
@@ -18,10 +18,7 @@ from django.urls import path
 from django.contrib import admin
 from django.urls import path, include
 
-from . import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('', include('absences.urls')),
 ]
