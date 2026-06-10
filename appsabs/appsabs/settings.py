84e00b95b2f7ebd4be0c9a9c9604147635f4baf8
Lommé_Sichard <98736234+0PPA-art@users.noreply.github.com>
2026-06-10 09:58:41 +0200

    Update ALLOWED_HOSTS for development environment
    mise a jour

--- appsabs/appsabs/settings.py
+++ appsabs/appsabs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cib5@rft$-v0)zu8$4-kl9zokujpt-6^y)knmxbm5c+rm)5fwo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','10.128.208.26','localhost',]
 
 
 # Application definition
